19c5d4fd58c19f6eb2efb059247e44822d0e05cf
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-13 20:53:23 +0600

    update

--- src/pages/BookingRow.jsx
+++ src/pages/BookingRow.jsx
@@ -1,25 +1,33 @@
-const BookingRow = ({booking}) => {
-    const {img, date, email, price, services_title} = booking;
+import Swal from "sweetalert2";
+
+const BookingRow = ({booking,handleDelete, handleBookingConfirm}) => {
+    const {img, date, email, price, services_title, _id, status} = booking;
     console.log(booking)
+
+
+
+
+
     return (
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+        <button onClick={()=>handleDelete(_id)} className="btn btn-circle btn-outline btn-sm">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+</button>
         </th>
-        <td>
-          <div className="flex items-center space-x-3">
+          <td>
+
             <div className="avatar">
               <div className="mask mask-squircle w-12 h-12">
                {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
               </div>
             </div>
+          </td>
+        <td>
             <div>
               <div className="font-bold">{services_title}</div>
-              <div className="text-sm opacity-50">United States</div>
             </div>
-          </div>
+         
         </td>
         <td>
          {date}
@@ -27,7 +35,7 @@ const BookingRow = ({booking}) => {
         <td>{email}</td>
         <td>${price}</td>
         <th>
-          <button className="btn btn-ghost btn-xs">details</button>
+         { status === "confirm" ? <span className="text-primary font-bold">Confirmed</span> : <button onClick={()=>handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Confirm</button>}
         </th>
       </tr>
     
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import BookingRow from './BookingRow';
+import Swal from 'sweetalert2';
 
 const Bookings = () => {
 
@@ -12,9 +13,70 @@ const Bookings = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setBookings(data))
-    },[])
+    },[url])
 
     console.log(bookings)
+
+
+    
+    const handleDelete =(id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+              fetch(`http://localhost:5000/bookings/${id}`,{
+                method: 'DELETE',
+              })
+              .then(res => res.json())
+              .then(data => {console.log(data)
+                if (data.deletedCount > 0) {
+                    // Swal.fire(
+                    //     'Deleted!',
+                    //     'Your file has been deleted.',
+                    //     'success'
+                    //   ) 
+                      const remaining = bookings.filter(booking => booking._id !== id)
+                      setBookings(remaining)
+                }
+            })
+            }
+          })
+    }
+
+    const handleBookingConfirm = (id) => {
+          fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+              'content-type' : 'application/json',
+            },
+            body: JSON.stringify({status : 'confirm'})
+          })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            if (data.modifiedCount > 0) {
+              const remaining = bookings.filter(booking => booking._id !== id);
+              const update = bookings.find(booking => booking._id === id);
+              update.status = 'confirm';
+              const newBookings = [update, ...remaining];
+              setBookings(newBookings)
+            }
+          })
+    }
+
+
+
 
     return (
         <div>
@@ -29,14 +91,16 @@ const Bookings = () => {
             <input type="checkbox" className="checkbox" />
           </label> */}
         </th>
+        <th>image</th>
         <th>Service</th>
         <th>Date</th>
         <th>Email</th>
         <th>Price</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-    {bookings.map((booking, index) => <BookingRow booking={booking} key={index}></BookingRow>)}
+    {bookings.map((booking, index) => <BookingRow booking={booking} key={index} handleDelete={handleDelete} handleBookingConfirm={handleBookingConfirm}></BookingRow>)}
             
     </tbody>
   </table>
