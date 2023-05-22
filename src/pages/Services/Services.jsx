399a032847d79aa72db7a1acbac685906bc95f44
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-22 22:49:59 +0600

    logo, name, search, dort

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,16 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import ServicesCard from "./ServicesCard";
 
 const Services = () => {
     const [services, setServices] = useState([]);
     const [asc, setAsc] = useState(true);
+    const [search, setSearch] = useState('');
+    const searchRef = useRef(null)
     
     useEffect(()=>{
-fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'desc'}`)
+fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? 'asc' : 'desc'}`)
 .then(res => res.json())
 .then(data => setServices(data))
-    },[asc])
+    },[asc, search])
+
+    const handleSearch =()=>{
+console.log(searchRef.current.value)
+setSearch(searchRef.current.value)
+    }
 
 
     return (
@@ -21,6 +28,14 @@ fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'desc'}`)
                 <p className="my-5">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
                 <button className="btn"
                 onClick={()=> setAsc(!asc)}>{asc ? "low to high" : "high to low"}</button>
+           <div className="form-control">
+  <div className="input-group flex justify-center mt-3">
+    <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+    <button onClick={handleSearch} className="btn btn-square">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+</div>
             </div>
             <div className="grid grid-cols-1 mx-auto lg:gap-6 md:grid-cols-2 lg:grid-cols-3 my-9">
                 {services.map(service => <ServicesCard key={service._id} service={service}></ServicesCard>)}
